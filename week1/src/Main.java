d55f6f9236aac9c7429757a07b0109db4037c67e
unknown <rizkiarkant55@gmail.com>
2025-07-11 14:41:31 +0700

    week1 80% kelar

--- week1/src/Main.java
+++ week1/src/Main.java
@@ -8,58 +8,76 @@ public class Main {
     public static void main(String[] args) {
 
         // inisiasi array secara langsung sak valuenya
-        int[] daftarAngka = { 10, 20, 30, 40, 50 };
+        int[] daftarAngka = { 5, 3, 7, 3, 9, 3, 2, 8, 3, 1 };
+        String[] daftarNama = {
+                "ekik", "Dani", "Ilham", "EKIK", "Dian", "eKiK", "Rizki"
+        };
+
         Scanner s = new Scanner(System.in);
         Scanner k = new Scanner(System.in);
         boolean start = true;
+        int hasilInt;
+        int[] hasilArrayInt;
+        int inputanInt;
+        String inputanStr;
+        String hasilStr;
+        String[] hasilArrayString;
 
         while (start) {
             Helper.menu();
-            int input = s.nextInt();
-            switch (input) {
+            inputanInt = Helper.cekInputInt(s, "Pilih Menu ", ">>> ");
+            switch (inputanInt) {
                 case 0:
                     System.out.println("Process Exit...");
                     start = false;
                     break;
 
                 case 1:
-                    System.out.println("isi array angka :");
-
-                    for (int i = 0; i < daftarAngka.length; i++) {
-                        System.out.println("index ke - " + daftarAngka[i]);
-                    }
-
-                    System.out.println("Index Pertama : " + daftarAngka[0]);
-                    System.out.println("Index Pertama : " + daftarAngka[daftarAngka.length - 1]);
-
+                    System.out.println("isi array integer :");
+                    Helper.printArrayInt(daftarAngka);
                     break;
                 case 2:
-
                     System.out.println("Reverse Array : ");
-                    for (int i = daftarAngka.length - 1; i >= 0; i--) {
+                    int[] hasilArr = Helper.reversedArray(daftarAngka);
 
-                        System.out.println("index ke : " + i + " -> " + daftarAngka[i]);
-                    }
+                    System.out.println("Hasil Reverse Array : ");
+                    Helper.printArrayInt(hasilArr);
                     break;
                 case 3:
-                    System.out.println("Linear Search : ");
-                    System.out.print("Masukan Inputan : ");
-                    if (!s.hasNextInt()) {
-                        System.out.println("Inputan Harus Berupa Angka  ");
-                        s.next();
-                        break;
-                    }
-
-                    int inputan = s.nextInt();
-                    int hasil = Helper.linearSearch(inputan, daftarAngka);
-
+                    inputanInt = Helper.cekInputInt(s, "Linear Search :", "Masukan Inputan : ");
+                    int hasil = Helper.linearSearch(inputanInt, daftarAngka);
                     if (hasil == -1) {
                         System.out.println("Inputan tidak ditemukan : " + hasil);
                         break;
                     }
 
-                    System.out.println("hasil dari linear search : " + hasil);
+                    System.out.println(
+                            "hasil dari linear search : " + "index ke - " + hasil + " = " + daftarAngka[hasil]);
 
+                    break;
+                case 4:
+                    inputanInt = Helper.cekInputInt(s, "Linear Search Multiple Data Integer : ", "Masukan Inputan : ");
+                    hasilArrayInt = Helper.multipleSearchInteger(inputanInt, daftarAngka);
+                    for (int i = 0; i < hasilArrayInt.length; i++) {
+                        if (hasilArrayInt[i] != 0) {
+                            System.out.println("index ke - " + i + " -> " + daftarAngka[i]);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    inputanStr = Helper.cekInputString(k, "Linear Search ", "Masukan Inputan :");
+                    System.out.println(inputanStr);
+                    hasilArrayInt = Helper.multipleSearchString(inputanStr, daftarNama);
+                    for (int i = 0; i < hasilArrayInt.length; i++) {
+                        System.out.println("index ke - " + i + " -> " + daftarNama[i]);
+                    }
+                    break;
+                case 6:
+                    inputanStr = Helper.cekInputString(k, "String Reversal ", "Masukan Inputan :");
+                    System.out.println(inputanStr);
+                    hasilArrayString = Helper.reversedString(inputanStr);
+                    Helper.printArrayString(hasilArrayString);
                     break;
                 default:
                     System.out.println("Inputan Tidak Valid ");
--- week1/src/utils/Helper.java
+++ week1/src/utils/Helper.java
@@ -1,15 +1,88 @@
 // src/utils/Helper.java
 package utils;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Helper {
     public static void menu() {
         System.out.println("\nMenu : ");
         System.out.println("1. Basic Array");
         System.out.println("2. Reverse Array");
-        System.out.println("3. Linear Search");
+        System.out.println("3. Linear Search Integer");
         System.out.println("4. Linear Search Multiple Match");
-        System.out.println("0. Exit ");
-        System.out.print(">>> ");
+        System.out.println("5. Linear Search String");
+        System.out.println("6. String Reversal");
+
+        System.out.println("0. Exit\n");
+    }
+
+    // selama dia true jalanin whilenya lalu return dan pakai next() buat "bersihin"
+    // inputan
+    public static int cekInputInt(Scanner s, String title, String teks) {
+        while (true) {
+            System.out.println(title);
+            System.out.print(teks);
+            if (s.hasNextInt()) {
+                return s.nextInt();
+            } else {
+                System.out.println("\nInput harus angka!");
+                s.next();
+            }
+        }
+    }
+
+    public static String cekInputString(Scanner s, String title, String teks) {
+        while (true) {
+            System.out.println(title);
+            System.out.print(teks);
+            if (s.hasNextLine()) {
+                return s.nextLine();
+            } else {
+                System.out.println("\nInput harus string!");
+                s.next();
+            }
+        }
+    }
+
+    public static void printArrayInt(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("index ke - " + i + " -> " + data[i]);
+        }
+
+        System.out.println("Index Pertama  : " + data[0]);
+        System.out.println("Index Terakhir : " + data[data.length - 1]);
+
+    }
+
+    public static void printArrayString(String[] data) {
+        System.out.println("isi array string :");
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " - ");
+        }
+    }
+
+    // ambil param array int terus reverse
+    // contoh : 5, 3, 7, 3, 9, 3, 2, 8, 3, 1
+    public static int[] reversedArray(int[] data) {
+        int[] reversedArray = new int[data.length];
+        int kanan = reversedArray.length - 1;
+        int kiri = 0;
+        System.out.println("Panjang data array " + reversedArray.length);
+        System.out.println("Debugging...");
+        while (kiri <= reversedArray.length - 1) {
+
+            reversedArray[kiri] = data[kanan];
+            // proses debug biar enak liatnya
+            System.out.println(
+                    "index kiri : " + kiri + " -> " + reversedArray[kiri] + " - " + "index kanan : " + kanan + " -> "
+                            + data[kanan]);
+            kiri++;
+            kanan--;
+
+        }
+        return reversedArray;
     }
 
     public static int linearSearch(int target, int[] data) {
@@ -21,6 +94,47 @@ public class Helper {
             }
         }
         return -1;
+    }
+
+    public static int[] multipleSearchInteger(int target, int[] data) {
+        int[] indexHasil = new int[data.length];
+        for (int i = 0; i < indexHasil.length; i++) {
+            if (target == data[i]) {
+                indexHasil[i] = i;
+            }
+        }
+        return indexHasil;
+    }
+
+    public static int[] multipleSearchString(String target, String[] data) {
+        int[] indexHasil = new int[data.length];
+        for (int i = 0; i < indexHasil.length; i++) {
+            if (data[i].equalsIgnoreCase(target)) {
+                indexHasil[i] = i;
+            }
+        }
+
+        return indexHasil;
+    }
+
+    public static String[] reversedString(String data) {
+        String[] temp = data.split("");
+        String[] reversedString = new String[temp.length];
+        int kanan = temp.length - 1;
+        int kiri = 0;
+        System.out.println("Panjang data array string " + temp.length);
+        System.out.println("Debugging...");
+        while (kiri <= reversedString.length - 1) {
+            reversedString[kiri] = temp[kanan];
+            // proses debug biar enak liatnya
+            System.out.println(
+                    "index kiri : " + kiri + " -> " + reversedString[kiri] + " - " + "index kanan: " + kanan + " -> "
+                            + temp[kanan]);
+            kiri++;
+            kanan--;
+
+        }
+        return reversedString;
     }
 
 }
